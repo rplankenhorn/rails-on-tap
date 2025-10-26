4623da6d322fde932147f55fab5c5603519bbbb1
Robbie Plankenhorn <rplankenhorn@gmail.com>
2025-10-26 18:59:11 -0400

    Configure MainActivity to launch TapListActivity
    - Update MainActivity to immediately launch TapListActivity
    - Register TapListActivity in AndroidManifest
    - Update theme to NoActionBar with transparent status bar
    - Set up for native UI experience

--- android/app/src/main/java/com/railsontap/MainActivity.kt
+++ android/app/src/main/java/com/railsontap/MainActivity.kt
@@ -1,13 +1,16 @@
 package com.railsontap
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
         
-        // TODO: Initialize Turbo navigation
+        // Launch the tap list activity
+        val intent = Intent(this, TapListActivity::class.java)
+        startActivity(intent)
+        finish()
     }
 }
