5d62cc959a41979f435836afc91c1459b76f6ce7
Robbie Plankenhorn <rplankenhorn@gmail.com>
2025-10-26 13:07:16 -0400

    refactor: simplify MainActivity for initial setup
    - Remove Hotwire-specific code temporarily
    - Use basic AppCompatActivity
    - Prepare for future Turbo Native integration
    - Enables successful compilation

--- android/app/src/main/java/com/railsontap/MainActivity.kt
+++ android/app/src/main/java/com/railsontap/MainActivity.kt
@@ -2,24 +2,12 @@ package com.railsontap
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import dev.hotwire.android.nav.HotwireNavRule
-import dev.hotwire.android.navigation.HotwireActivity
-import java.net.URL
 
-class MainActivity : HotwireActivity() {
+class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-    }
-
-    override val startLocation: URL
-        get() = URL("http://localhost:3000")
-
-    override fun onCreateNavRules(): List<HotwireNavRule> {
-        return listOf(
-            HotwireNavRule(
-                "http://localhost:3000/**",
-                destinationIdentifier = "web"
-            )
-        )
+        setContentView(R.layout.activity_main)
+        
+        // TODO: Initialize Turbo navigation
     }
 }
